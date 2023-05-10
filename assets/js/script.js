888792a829e3a569c8434b59f70cec70e6504df4
Nick JDA <armstrongnick1999@gmail.com>
2023-05-10 19:37:33 -0400

    copied timeblock element for each hour required in HTML, added variables for class selectors in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var pastTime = $('.past');
+var presentTime = $('.present');
+var futureTime = $('.future');
+
 //$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,6 +26,8 @@
   //
 //});
 
+
+
 // TODO: Add code to display the current date in the header of the page. Done!
 var currentDayEl = $('#currentDay');
 
@@ -31,3 +38,4 @@ function dayCurrent() {
 dayCurrent();
 
 setInterval(dayCurrent, 1000);
+
